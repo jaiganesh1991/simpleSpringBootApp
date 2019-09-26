7bd33b977a510710e5c06533259e68c3b5f8526c
jaiganesh1991 <jaiganesh1991@gmail.com>
2019-09-26 22:46:40 +0530

    Simple spring Boot App to say Hi

---
+++ src/main/java/com/jaiganesh/simpleSpringBootApp/simpleSpringBootAPI.java
@@ -0,0 +1,16 @@
+package com.jaiganesh.simpleSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class simpleSpringBootAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(simpleSpringBootAPI.class, args);
+
+	}
+
+}
+ 
